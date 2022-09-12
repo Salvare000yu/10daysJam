01982eaa9688d3a34fc289095d77a087573cc815
sasanonoha <k020g1406@g.neec.ac.jp>
2022-09-12 13:32:57 +0900

    自機のオブジェクトの追加

--- Solution/RailShoot.cpp
+++ Solution/RailShoot.cpp
@@ -123,7 +123,7 @@ RailShoot::RailShoot()
 	// --------------------
 	// 自機関連
 	// --------------------
-	playerModel(std::make_unique<ObjModel>("Resources/box", "box")),
+	playerModel(std::make_unique<ObjModel>("Resources/player", "player")),
 	playerBulModel(std::make_unique<ObjModel>("Resources/sphere", "sphere", 0U, true)),
 	playerHp(20u),
 
